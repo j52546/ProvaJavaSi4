124a9d8b7c8f39f2de76309c0415ebeb5397db3d
j52546 <j52546@gmail.com>
2017-10-04 21:21:38 -0300

    Prova Completo

--- Prova/src/br/edu/univas/si4/Prova.java
+++ Prova/src/br/edu/univas/si4/Prova.java
@@ -6,6 +6,8 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -16,7 +18,7 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 
-public class Prova extends JFrame {
+public class Prova extends JFrame implements ActionListener{
 
 	private static final long serialVersionUID = 1L;
 	
@@ -25,7 +27,7 @@ public class Prova extends JFrame {
 	public Prova() {
 		
 		this.setTitle("Prova");
-		this.setSize(800,600);
+		this.setSize(500,450);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		setContentPane();
@@ -47,8 +49,8 @@ public class Prova extends JFrame {
 	private void addComponets() {
 		
 		JPanel westPanel = new JPanel();
-		westPanel.setPreferredSize(new Dimension(235, 120));
-		westPanel.setBackground(new Color(224, 224, 224));
+		westPanel.setPreferredSize(new Dimension(150, 120));
+		westPanel.setBackground(new Color(131, 139, 131));
 		populatewestPanel(westPanel);
 		panel.add(westPanel, BorderLayout.WEST);
 		
@@ -61,14 +63,71 @@ public class Prova extends JFrame {
 	
 	private void populatewestPanel(JPanel westPanel){
 		
-		JLabel nameLabel = new JLabel();
-		nameLabel.setPreferredSize(new Dimension(300, 30));
-		nameLabel.setText("Nome: ");
-		westPanel.add(nameLabel);
+		JButton button01 = new JButton();
+		button01.setText("Botão 01");
+		button01.setPreferredSize(new Dimension(100, 30));
+		button01.addActionListener(this);
+		westPanel.add(button01);
+		
+		JButton button02 = new JButton();
+		button02.setText("Botão 02");
+		button02.setPreferredSize(new Dimension(100, 30));
+		button02.addActionListener(this);
+		westPanel.add(button02);
+		
+		JButton button03 = new JButton();
+		button03.setText("Botão 03");
+		button03.addActionListener(this);
+		button03.setPreferredSize(new Dimension(100, 30));
+		westPanel.add(button03);
+
+		JButton button04 = new JButton();
+		button04.setText("Botão 04");
+		button04.setPreferredSize(new Dimension(100, 30));
+		button04.addActionListener(this);
+		westPanel.add(button04);
+
+		JButton button05 = new JButton();
+		button05.setText("Botão 05");
+		button05.setPreferredSize(new Dimension(100, 30));
+		button05.addActionListener(this);
+		westPanel.add(button05);
+		
+		JButton button06 = new JButton();
+		button06.setText("Botão 06");
+		button06.setPreferredSize(new Dimension(100, 30));
+		button06.addActionListener(this);
+		westPanel.add(button06);
+		
+		JButton button07 = new JButton();
+		button07.setText("Botão 07");
+		button07.setPreferredSize(new Dimension(100, 30));
+		button07.addActionListener(this);
+		westPanel.add(button07);
+		
+		JButton button08 = new JButton();
+		button08.setText("Botão 08");
+		button08.setPreferredSize(new Dimension(100, 30));
+		button08.addActionListener(this);
+		westPanel.add(button08);
+		
+		JButton button09 = new JButton();
+		button09.setText("Botão 09");
+		button09.setPreferredSize(new Dimension(100, 30));
+		button09.addActionListener(this);
+		westPanel.add(button09);
+		
+		JButton button10 = new JButton();
+		button10.setText("Botão 10");
+		button10.setPreferredSize(new Dimension(100, 30));
+		button10.addActionListener(this);
+		westPanel.add(button10);
+		
 	}
 	
 	private void populateCenterPanel(JPanel centerPanel) {
 		
+		centerPanel.setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 		
 		JLabel nameLabel = new JLabel();
@@ -77,32 +136,32 @@ public class Prova extends JFrame {
 		gbc.gridy = 0;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 		gbc.anchor = GridBagConstraints.LINE_END;
-		gbc.insets = new Insets(10,10,10,10);
+		gbc.insets = new Insets(15, 15, 10, 15);
 		centerPanel.add(nameLabel, gbc);
-	
-		JTextField nomeTextField = new JTextField();
+		
+		JTextField nameTextField = new JTextField();
 		gbc.gridx = 1;
 		gbc.gridy = 0;
 		gbc.weightx = 1.0;
-		gbc.insets = new Insets(15,0,10,15);
+		gbc.insets = new Insets(15, 0, 10, 15); 
 		gbc.anchor = GridBagConstraints.CENTER;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
-		centerPanel.add(nomeTextField, gbc);
-		
+		centerPanel.add(nameTextField, gbc);
+
 		JLabel emailLabel = new JLabel();
-		emailLabel.setText("E-Mail: ");
+		emailLabel.setText("E-mail: ");
 		gbc.gridx = 0;
 		gbc.gridy = 1;
 		gbc.weightx = 0;
-		gbc.insets = new Insets(15,15,10,15);
+		gbc.insets = new Insets(15, 15, 10, 15);
 		gbc.anchor = GridBagConstraints.LINE_END;
 		centerPanel.add(emailLabel, gbc);
-		
+
 		JTextField emailTextField = new JTextField();
 		gbc.gridx = 1;
 		gbc.gridy = 1;
 		gbc.weightx = 1.0;
-		gbc.insets = new Insets(15,0,10,15);
+		gbc.insets = new Insets(15, 0, 10, 15); 
 		gbc.anchor = GridBagConstraints.LINE_START;
 		centerPanel.add(emailTextField, gbc);
 		
@@ -111,15 +170,15 @@ public class Prova extends JFrame {
 		gbc.gridx = 0;
 		gbc.gridy = 2;
 		gbc.gridwidth = 2;
-		gbc.insets = new Insets(15,15,15,15);
+		gbc.insets = new Insets(15, 15, 15, 15); 
 		gbc.fill = GridBagConstraints.NONE;
 		gbc.anchor = GridBagConstraints.CENTER;
 		centerPanel.add(saveButton, gbc);
 		
 		Object[][] rowDate = {
-				{"","",""},
-				{"","",""},
-				{"","",""},
+				{"João","111.111.111-11","joao@gmail.com"},
+				{"Maria","222.222.222-22","maria@gmail.com"},
+				{"Jose","333.333.333-33","jose@gmil.com"},
 		};
 		Object[] columnNames = {
 				"Nome",
@@ -130,17 +189,23 @@ public class Prova extends JFrame {
 		JTable dateTable = new JTable(rowDate,columnNames);
 		dateTable.setFillsViewportHeight(true);
 		JScrollPane tableScrollPane = new JScrollPane(dateTable);
-		tableScrollPane.setMinimumSize(new Dimension(70, 70));
+		tableScrollPane.setMinimumSize(new Dimension(100,100));
 		tableScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		tableScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		tableScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
 		
 		gbc.gridx = 0;
-		gbc.gridy = 3;
-		gbc.gridwidth = 2;
+		gbc.gridy = 4;
+		gbc.gridwidth = 3;
 		gbc.weightx = 1.0;
 		gbc.fill = GridBagConstraints.BOTH;
 		gbc.anchor = GridBagConstraints.CENTER;
 		centerPanel.add(tableScrollPane, gbc);
 		
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		System.out.println("Eu sou o responsavel pelo meu futuro !");
 	}
 }
